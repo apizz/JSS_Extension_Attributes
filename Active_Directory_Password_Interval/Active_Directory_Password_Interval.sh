1fc36df9672b60bab1f3faf6d1f69c17b916b4df
AP Orlebeke <aporlebeke@gmail.com>
2016-07-16 19:13:41 -0400

    Added script comments

--- Active_Directory_Password_Interval/Active_Directory_Password_Interval.sh
+++ Active_Directory_Password_Interval/Active_Directory_Password_Interval.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+###########
+#
+# Proactive EA that checks AD pass interval. If not set to 0, attempts to.  Not being set
+# to 0 can cause problems for users where password change policies are already in place.
+#
+###########
+
 adpass=`/usr/sbin/dsconfigad -show | grep "Password" | sed -n -e 's/^.*\=\ //p'`
 
 if [ "$adpass" != "0" ]; then
@@ -11,7 +18,7 @@ if [ "$adpass" != "0" ]; then
 		echo "AD Password Interval: Successfully set to 0"
 		result="OK"
 	else
-		result="Failed to set AD Password Interval to 0"
+		result="AD Password Interval: Failed to set AD Password Interval to 0"
 	fi
 else
 	result="OK"
