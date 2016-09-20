cf5583100224301c2dcfe1b72b58738feb5f68f3
AP Orlebeke <aporlebeke@gmail.com>
2016-09-20 12:44:00 -0400

    Initial commit

---
+++ TeamViewer_ID_Collection/TeamViewer_ID_Collection.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEAMVIEWER="/Applications/TeamViewer.app"
+TEAMVIEWER_HOST="/Applications/TeamViewerHost.app"
+PLIST="/Library/Preferences/com.teamviewer.teamviewer.preferences.plist"
+
+if [ -d "$TEAMVIEWER_HOST" ]; then
+    result=`/usr/bin/defaults read "$PLIST" ClientID`
+elif [ -d "$TEAMVIEWER" ]; then
+    result=`/usr/bin/defaults read "$PLIST" ClientID`
+else
+    result="TeamViewer Host Not Installed"
+fi
+
+/bin/echo "<result>$result</result>"
+
+exit
