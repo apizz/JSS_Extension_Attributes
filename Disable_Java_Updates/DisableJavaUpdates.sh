a12154c7ad5da09faa6f9727c2de6833ba06b97f
AP Orlebeke <aporlebeke@gmail.com>
2016-10-07 16:57:35 -0400

    Initial commit

---
+++ Disable_Java_Updates/DisableJavaUpdates.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PLIST="/Library/Preferences/com.oracle.java.Java-Updater.plist"
+LOG="/Library/Logs/TMSTech/TMSJava.log"
+
+writelog () {
+    /bin/echo $(date) ${1}
+    /bin/echo $(date) ${1} >> $LOG
+}
+
+if [ ! -f "$PLIST" ]; then
+	result="Java Not Installed"
+	
+elif [ -f "$PLIST" ] && [ ! "$(/usr/bin/defaults read $PLIST JavaAutoUpdateEnabled)" = "0" ]; then 
+	writelog "Java Auto Update Checks Enabled. Disabling ..."
+	
+	/usr/bin/defaults write "$PLIST" JavaAutoUpdateEnabled -bool false
+	
+	if [ "$(/usr/bin/defaults read $PLIST JavaAutoUpdateEnabled)" = "0" ]; then
+		writelog "Java Updates Disabled"
+		result="Updates Disabled"
+		
+	else
+		writelog "Java Updates Not Disabled"
+		result="Updates Not Disabled"
+		
+	fi
+else
+	writelog "Jave Updates Already Disabled"
+	result="Updates Already Disabled"
+fi
+
+/bin/echo "<result>$result</result>"
+
+exit
