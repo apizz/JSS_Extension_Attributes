65bbbe81647b3abb55c0db03bda84ffc99560c5a
AP Orlebeke <aporlebeke@gmail.com>
2016-10-18 22:15:41 -0400

    Update if statement
    Condensed to if || or if, rather than if, elif, else

--- TeamViewer_ID_Collection/TeamViewer_ID_Collection.sh
+++ TeamViewer_ID_Collection/TeamViewer_ID_Collection.sh
@@ -4,9 +4,7 @@ TEAMVIEWER="/Applications/TeamViewer.app"
 TEAMVIEWER_HOST="/Applications/TeamViewerHost.app"
 PLIST="/Library/Preferences/com.teamviewer.teamviewer.preferences.plist"
 
-if [ -d "$TEAMVIEWER_HOST" ]; then
-    result=`/usr/bin/defaults read "$PLIST" ClientID`
-elif [ -d "$TEAMVIEWER" ]; then
+if [ -d "$TEAMVIEWER_HOST" ] || [ -d "$TEAMVIEWER" ]; then
     result=`/usr/bin/defaults read "$PLIST" ClientID`
 else
     result="TeamViewer Host Not Installed"
