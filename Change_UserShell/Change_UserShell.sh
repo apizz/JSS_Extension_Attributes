0b1be204c414b9a7cf11fad8ba80f26668f43d60
AP Orlebeke <aporlebeke@gmail.com>
2016-11-12 17:07:48 -0500

    Add EA result with user and UserShell on new lines

--- Change_UserShell/Change_UserShell.sh
+++ Change_UserShell/Change_UserShell.sh
@@ -7,6 +7,7 @@ USER=('root'
 'ladmin'
 'arduser'
 'casperadmin')
+RESULT=()
 
 for ((i = 0; i < "${#USER[@]}"; i++)); do	
 	# Gets UserShell given USER array entry
@@ -22,14 +23,25 @@ for ((i = 0; i < "${#USER[@]}"; i++)); do
 			/usr/bin/chsh -s /usr/bin/false "${USER[$i]}"
 			
 			if [ $? = 0 ]; then
-				/bin/echo "Successfully changed UserShell for ${USER[$i]} from ${SHELL} to /usr/bin/false!"
+				NEWSHELL=`/usr/bin/dscacheutil -q user -a name ${USER[$i]} | /usr/bin/grep shell | /usr/bin/awk '{print $2}'
+				
+				/bin/echo "Successfully changed UserShell for ${USER[$i]} from ${SHELL} to {$NEWSHELL}!"
+				
+				RESULT+=("${USER[$i]}:${NEWSHELL}")
 			else
 				/bin/echo "Failed to change UserShell for ${USER[$i]} from ${SHELL} to /usr/bin/false."
+				
+				RESULT+=("${USER[$i]}:${SHELL}")
 			fi
 		else
 			/bin/echo "${USER[$i]} UserShell is ${SHELL}. Login is already disabled!"
+			RESULT+=("${USER[$i]}:${SHELL}")
 		fi
+	else
+		RESULT+=("${USER[$i]}:${SHELL}")
 	fi
 done
+
+/bin/echo "<result>$(printf "%s\n" ${RESULT[@]})</result>"
 
 exit
