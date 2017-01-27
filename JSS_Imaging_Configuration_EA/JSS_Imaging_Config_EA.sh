da50e21a561c0767ee507fc053c2a96505b0f19e
AP Orlebeke <aporlebeke@gmail.com>
2017-01-27 16:00:42 -0500

    Add link to bash script

--- JSS_Imaging_Configuration_EA/JSS_Imaging_Config_EA.sh
+++ JSS_Imaging_Configuration_EA/JSS_Imaging_Config_EA.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+########################
+# Created by AP Orlebeke - 1/27/17
+#
+# For use with my JSS_Imaging_Config_Log bash script for writing the imaging configuration used to image
+# a machine.  This can bash script can be found here:
+# https://github.com/apizz/Mac_Scripts/blob/master/JSS_Imaging_Configuration_Log/JSS_Imaging_Config_Log.sh
+########################
+
 LOG="/var/log/imagingconfig.log"
 RESULT=$(/usr/bin/tail -1 "$LOG")
 
