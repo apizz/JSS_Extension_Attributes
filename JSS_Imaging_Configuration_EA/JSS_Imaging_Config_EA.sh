986489c527f57f2065abd5a44841b00dab3dfb0f
AP Orlebeke <aporlebeke@gmail.com>
2017-05-03 16:54:06 -0400

    Fix typo

--- JSS_Imaging_Configuration_EA/JSS_Imaging_Config_EA.sh
+++ JSS_Imaging_Configuration_EA/JSS_Imaging_Config_EA.sh
@@ -4,7 +4,7 @@
 # Created by AP Orlebeke - 1/27/17
 #
 # For use with my JSS_Imaging_Config_Log bash script for writing the imaging configuration used to image
-# a machine.  This can bash script can be found here:
+# a machine.  This bash script can be found here:
 # https://github.com/apizz/Mac_Scripts/blob/master/JSS_Imaging_Configuration_Log/JSS_Imaging_Config_Log.sh
 ########################
 
